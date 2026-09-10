408a3d3734e0e99c7de899fab31f33dd022cc1f7
Atomic Shelf <powerpages30@gmail.com>
2026-09-10 22:31:09 +0300

    Add standardEbooksProvider to book repository

--- server/books/catalog.ts
+++ server/books/catalog.ts
@@ -1,10 +1,12 @@
 import { openLibraryProvider } from "./providers/openlibrary";
 import { builtinProvider } from "./providers/builtin";
 import { publicDomainProvider } from "./providers/publicDomain";
+import { standardEbooksProvider } from "./providers/standardEbooks";
 import { BookRepository } from "./repository";
 
 export const bookRepository = new BookRepository([
   builtinProvider,
   publicDomainProvider,
   openLibraryProvider,
+  standardEbooksProvider,
 ]);
